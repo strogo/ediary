f26f14cb9b7045ffc4eb3103c0eb5ac21a356398
Igor Kalnitsky <igor@kalnitsky.org>
2012-05-05 14:03:36 +0300

    Change license header comment.

--- views.py
+++ views.py
@@ -1,22 +1,21 @@
 # coding: utf-8
-'''
-  Copyright 2011 Igor Kalnitsky <igor@kalnitsky.org>
 
-  This file is part of eDiary.
-
-  eDiary is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  eDiary is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with eDiary.  If not, see <http://www.gnu.org/licenses/>.
-'''
+# Copyright 2011 Igor Kalnitsky <igor@kalnitsky.org>
+#
+# This file is part of eDiary.
+#
+# eDiary is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# eDiary is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with eDiary.  If not, see <http://www.gnu.org/licenses/>.
 
 import app_settings
 from models import Article, Category
